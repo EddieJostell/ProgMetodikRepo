0f6f710a5ce8c109548e8c641486a0c57f039f88
Eduardo Jostello <Eduardo Jostello>
2016-10-10 13:24:59 +0200

    ProgMetodik Algoritmer

---
+++ Algoritmer/script.js
@@ -0,0 +1,30 @@
+// ALGORITMER & DATASTRUKTURER
+
+//ÖVNINGAR
+
+//SIMULERA EN STACK I JAVASCRIPT
+/*
+var arr = [2,3,5,6,8];
+arr.pop(); //Ta bort ett värde ur arrayen
+arr.push(55); //Pusha in ett värde sist i arrayen.
+arr.unshift(67); // Lägg till ett värde först i arrayen.
+
+ console.log(arr);
+*/
+
+
+// SIMLUERA EN KÖ I JAVASCRIPT
+/*
+var arr = [8,5,9,11,56];
+
+arr.shift(); // Ta bort först i kön.
+arr.unshift(77); // Lägg till först i kö9n
+arr.push(99); //Lägg till sist i kön.
+console.log(arr);
+*/
+
+
+// SORTERA EN ARRAY MED BUBBLESORT
+
+// SE MEDFÖLJANDE PAINT DOC
+
